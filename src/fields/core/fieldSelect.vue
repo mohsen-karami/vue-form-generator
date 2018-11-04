1c4ffd1f36d3474058f6dbba6788c5d9a17dee63
serverfire <info@serverfire.net>
2018-11-04 14:43:58 +0330

    better css for selectbox

--- src/fields/core/fieldSelect.vue
+++ src/fields/core/fieldSelect.vue
@@ -1,22 +1,19 @@
 <template>
-	<div class="btn-group">
-		<li @click="toggleMenu()" class="dropdown-toggle" v-if="selectedOption.name !== undefined">
-			{{ selectedOption.name }}
-			<span class="caret"></span>
-		</li>
-
-		<li @click="toggleMenu()" class="dropdown-toggle" v-if="selectedOption.name === undefined">
-			{{placeholderText}}
-			<span class="caret"></span>
-		</li>
-
-		<ul class="dropdown-menu" v-if="showMenu">
-			<li  v-for="item in items" :key="getItemValue(item)">
-				<a href="javascript:void(0)" @click="updateOption(item)">
-					{{ getItemName(item) }}
-				</a>
+	<div class="customSelect">
+		<div class="btn-group-select">
+			<li @click="toggleMenu()" class="dropdown-toggle-select" v-if="selectedOption.name !== undefined">
+				{{ text_truncate(selectedOption.name,30) }}
+				<span class="caret"></span>
 			</li>
-		</ul>
+
+			<ul class="dropdown-select" v-if="showMenu">
+				<li  v-for="item in items" :key="getItemValue(item)">
+					<a href="javascript:void(0)" @click="updateOption(item)">
+						{{ getItemName(item) }}
+					</a>
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
@@ -29,18 +26,16 @@ export default {
 	data() {
 		return {
 			selectedOption: {
-				name: "",
+				name: "Please select an item",
 			},
 			showMenu: false,
-			placeholderText: "Please select an item",
 		};
 	},
 	props: {
 		options: {
 			type: [Array, Object]
 		},
-		selected: {},
-		placeholder: [String]
+		selected: {}
 	},
 
 	computed: {
@@ -55,13 +50,20 @@ export default {
 			} else return this.groupValues(values);
 		}
 	},
-	mounted() {
-		if (this.placeholder)
-		{
-			this.placeholderText = this.placeholder;
-		}
-	},
 	methods: {
+		text_truncate(str, length, ending) {
+			if (length == null) {
+				length = 100;
+			}
+			if (ending == null) {
+				ending = "...";
+			}
+			if (str.length > length) {
+				return str.substring(0, length - ending.length) + ending;
+			} else {
+				return str;
+			}
+		},
 		updateOption(option) {
 			this.value = this.getItemValue(option);
 			this.selectedOption = option;
@@ -172,93 +174,85 @@ export default {
 };
 </script>
 
-<style>
-.btn-group {
-	min-width: 160px;
-	height: 40px;
-	position: relative;
-	margin: 10px 1px;
-	display: inline-block;
-	vertical-align: middle;
-}
-.btn-group a:hover {
-	text-decoration: none;
-}
-.dropdown-toggle {
-	color: #636b6f;
-	min-width: 160px;
-	padding: 10px;
-	text-transform: none;
-	font-weight: 300;
-	margin-bottom: 7px;
-	border: 0;
-	background-image: linear-gradient(#009688, #009688), linear-gradient(#D2D2D2, #D2D2D2);
-	background-size: 0 2px, 100% 1px;
-	background-repeat: no-repeat;
-	background-position: center bottom, center calc(100% - 1px);
-	background-color: transparent;
-	transition: background 0s ease-out;
-	float: none;
-	box-shadow: none;
-	border-radius: 0;
-}
-.dropdown-toggle:hover {
-	background: #e1e1e1;
-	cursor: pointer;
-}
-.dropdown-menu {
-	position: absolute;
-	top: 100%;
-	left: 0;
-	z-index: 1000;
-	float: left;
-	min-width: 160px;
-	padding: 5px 0;
-	margin: 2px 0 0;
-	list-style: none;
-	font-size: 14px;
-	text-align: left;
-	background-color: #fff;
-	border: 1px solid #ccc;
-	border-radius: 4px;
-	box-shadow: 0 6px 12px rgba(0, 0, 0, 0.175);
-	background-clip: padding-box;
-}
-.dropdown-menu > li > a {
-	padding: 10px 30px;
-	display: block;
-	clear: both;
-	font-weight: normal;
-	line-height: 1.6;
-	color: #333333;
-	white-space: nowrap;
-	text-decoration: none;
-}
-.dropdown-menu > li > a:hover {
-	background: #efefef;
-	color: #409FCB;
-}
-.dropdown-menu > li {
-	overflow: hidden;
-	width: 100%;
-	position: relative;
-	margin: 0;
-}
-.caret {
-	display: relative;
-	width: 0;
-	position: relative;
-	top: 10px;
-	height: 0;
-	margin-left: 2px;
-	vertical-align: middle;
-	border-top: 4px dashed;
-	border-top: 4px solid \9;
-	border-right: 4px solid transparent;
-	border-left: 4px solid transparent;
-	float: right;
-}
-li {
-	list-style: none;
+<style lang="scss">
+.customSelect{
+	.btn-group-select {
+		margin-bottom: 1rem;
+		padding: 5px;
+		display: block;
+		padding: 10px 17px 10px 10px;
+		font-size: 13px;
+		line-height: 1.42857143;
+		color: #555;
+		background-color: #fff;
+		background-image: none;
+		border: 1px solid #ccc;
+		border-radius: 4px;
+		-webkit-box-shadow: inset 0 1px 1px rgba(0,0,0,.075);
+		box-shadow: inset 0 1px 1px rgba(0,0,0,.075);
+		-webkit-transition: border-color .15s ease-in-out,-webkit-box-shadow .15s ease-in-out;
+		transition: border-color .15s ease-in-out,-webkit-box-shadow .15s ease-in-out;
+		transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+		transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out,-webkit-box-shadow .15s ease-in-out;
+	}
+	.btn-group-select a:hover {
+		text-decoration: none;
+	}
+	.dropdown-toggle-select {
+	}
+	.dropdown-toggle-select:hover {
+		background: #e1e1e1;
+		cursor: pointer;
+	}
+	.dropdown-select {
+		position: absolute;
+		top: 100%;
+		left: 0;
+		z-index: 1000;
+		float: left;
+		min-width: 160px;
+		padding: 5px 0;
+		margin: 2px 0 0;
+		list-style: none;
+		font-size: 14px;
+		text-align: left;
+		background-color: #fff;
+		border: 1px solid #ccc;
+		border-radius: 4px;
+		box-shadow: 0 6px 12px rgba(0, 0, 0, 0.175);
+		background-clip: padding-box;
+	}
+	.dropdown-select > li > a {
+		padding: 10px 30px;
+		display: block;
+		clear: both;
+		font-weight: 400;
+		color: #333;
+		font-size: 0.8rem;
+		width: 500px;
+	}
+	.dropdown-select > li > a:hover {
+		background: #efefef;
+		color: #409FCB;
+	}
+	.dropdown-select > li {
+		overflow: hidden;
+		width: 100%;
+		position: relative;
+		margin: 0;
+	}
+	.caret {
+		display: block;
+		width: 0;
+		top: 48px;
+		border-top: 4px dashed;
+		border-top: 4px solid\9;
+		border-right: 4px solid transparent;
+		border-left: 4px solid transparent;
+		float: right;
+	}
+	li {
+		list-style: none;
+	}
 }
 </style>
