dd227ccb426e1a43e13f621d5f06baaf8630cf54
Serverfire <info@serverfire.net>
2019-02-24 15:32:42 +0330

    change how checkbox works

--- src/fields/core/fieldCheckbox.vue
+++ src/fields/core/fieldCheckbox.vue
@@ -1,17 +1,36 @@
-<template lang="pug">
-	input(:id="getFieldID(schema)", type="checkbox", v-model="value", :autocomplete="schema.autocomplete", :disabled="disabled", :name="schema.inputName", :class="schema.fieldClasses", v-attributes="'input'")
+<template>
+	<span @click="onChange" class="checkbox" v-bind:class="{ active: value }" :id="getFieldID(schema)"  :disabled="disabled">
+		{{schema.label}}
+	</span>
 </template>
 
 <script>
 import abstractField from "../abstractField";
 
 export default {
-	mixins: [abstractField]
+	mixins: [abstractField],
+	methods: {
+		onChange($event){
+			this.value = !this.value;
+		}
+	}
 };
 </script>
 
 <style lang="scss">
-.vue-form-generator .field-checkbox input {
-	margin-left: 12px;
+.vue-form-generator .field-checkbox .checkbox{
+	color: #585858;
+    background-color: #fff;
+    padding: 5px;
+    border-radius: 5px;
+    border: 1px solid #686869;
+}
+.vue-form-generator .field-checkbox label{
+	display: none;
+}
+.vue-form-generator .field-checkbox .active{
+	color: #fff;
+	background-color: #337ab7;
+    border-color: #2e6da4;
 }
 </style>
